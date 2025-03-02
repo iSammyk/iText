9f73f5823d620ad7bd1e087d660a6deccb2bcdbf
iSammy <ayenisamuel155@gmail.com>
2025-03-02 22:28:56 +0000

    axios-update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api" // Localhost in development
+      : "https://itext-backend.onrender.com/api", // Deployed API in production
   withCredentials: true,
 });
